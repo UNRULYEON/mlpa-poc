eda42f23675fab05113f18334f8476d018224e10
Amar Kisoensingh <amar@kisoensingh.sh>
2021-05-11 00:03:57 +0200

    Initialized project

---
+++ server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = process.env.PORT || 8000
+
+app.use(express.static(path.join(__dirname, "./client")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/index.html"));
+});
+
+app.get("/health", (_, res) => res.sendStatus(200));
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
